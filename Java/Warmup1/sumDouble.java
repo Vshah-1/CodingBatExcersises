e82106568c89cd09a94ee699259c4babe10d9f1d
Vshah-1 <vshah1@protonmail.com>
2022-04-05 14:47:58 +0100

    Files

--- Java/Warmup1/sumDouble.java
+++ Java/Warmup1/sumDouble.java
@@ -1,2 +1,17 @@
-package Java.Warmup1;public class sumDouble {
+/* Problem Description:
+Given two int values, return their sum.
+Unless the two values are the same, then return double their sum.
+ */
+
+// Solution:
+
+package Java.Warmup1;
+
+public int sumDouble(int a, int b) {
+
+    if (a == b)
+        return ((a + b) * 2);
+
+    return a + b;
+
 }
