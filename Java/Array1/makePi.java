13259d74aa59a3d41ba437a09d01e891d943dee9
Vshah-1 <vshah1@protonmail.com>
2022-04-09 14:54:16 +0100

    Did a lot of Array work for coding bat

---
+++ Java/Array1/makePi.java
@@ -0,0 +1,13 @@
+/* Problem Description:
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+ */
+
+package Java.Array1;
+
+public int[] makePi() {
+
+        int[] nums = {3, 1, 4};
+        return nums;
+
+}
+
---
+++ Java/Array1/middleWay.java
@@ -0,0 +1,12 @@
+/* Problem Description:
+Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+ */
+
+package Java.Array1;
+
+public int[] middleWay(int[] a, int[] b) {
+
+        return new int[] {a[1], b[1]};
+
+}
+
